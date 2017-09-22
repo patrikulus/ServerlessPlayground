7195cae1820574e68a378f9bcd24c5210635c60d
Patryk Lotzwi <p.lotzwi@outlook.com>
2017-09-22 11:48:02 +0200

    Updated deployment script

--- ServerlessPlayground/DeployAll.ps1
+++ ServerlessPlayground/DeployAll.ps1
@@ -1,7 +1,13 @@
+function Get-FunctionName([string]$path) {
+    $splitted = $path.Split('\')
+    return $splitted[$splitted.Count -1]
+}
+
 $lambdaDir = Join-Path $PSScriptRoot ".\LambdaFunctions"
 $functions = Get-ChildItem $lambdaDir -Exclude "*.Tests"
 
 foreach ($function in $functions) {
     cd $function
-    dotnet lambda deploy-function 
+    $functionName = Get-FunctionName($function)
+    dotnet lambda deploy-function --name $functionName
 }
